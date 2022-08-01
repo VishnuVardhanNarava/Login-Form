b3457cfa87779b7f56de9ac57e262fbe8e26937c
VishnuVardhanNarava <vishnu.narava@gmail.com>
2022-08-01 19:25:26 +0530

    index.html and script.js files modified

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,20 @@
 let regexp = /[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g;
 
+document.getElementById("loginform").addEventListener("input", () =>{
+    let usrnamelth = document.getElementById("username").value.length;
+    let pswordlth = document.getElementById("password").value.length;
+    
+    if(usrnamelth == 9 && pswordlth == 9){
+        document.getElementById("submit").disabled = false;
+    }else{
+        document.getElementById("submit").disabled = true;
+    }
+})
+
 document.getElementById("loginform").addEventListener("submit", event =>{
     event.preventDefault();
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = event.target.UserName.value;
+    let password = event.target.Password.value;
     console.log(username);
     console.log(password);
 })
